87a52a56f50395ab47920cac90ffc723653dcefe
David Garcia Varas <davidga2@c1r7s4.42madrid.com>
2023-08-12 19:56:11 +0200

    rev_print

--- 1/rev_print.c
+++ 1/rev_print.c
@@ -1,18 +1,39 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 19:19:55 by davidga2          #+#    #+#             */
+/*   Updated: 2023/08/12 19:37:32 by davidga2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	main(int argc, char **argv)
 {
 	int i = 0;
+
 	if (argc == 2)
 	{
-		while (argv[1][i])
-			i++;
-		while (i > 0)
+		i = ft_strlen(argv[1]) - 1;
+		while (i >= 0)
 		{
-			write(1, &argv[1][i - 1], 1);
+			write (1, &argv[1][i], 1);
 			i--;
 		}
 	}
-	write (1, "\n", 1);
-	return (0);
+	return (write (1, "\n", 1), 0);
 }
