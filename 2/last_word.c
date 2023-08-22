2adb15213f3ef6d15cef43b1d15cda0c40bc0e25
David Garcia Varas <davidga2@c1r16s1.42madrid.com>
2023-08-22 17:31:45 +0200

    last_word

--- 2/last_word.c
+++ 2/last_word.c
@@ -1,43 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   last_word.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 16:36:17 by davidga2          #+#    #+#             */
+/*   Updated: 2023/08/22 17:09:29 by davidga2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int	ft_find_first(char **argv, int i)
+int ft_find_first_char(char *str, int i)
 {
-	while (i >= 0 && argv[1][i] != ' ')
+
+	while (i >= 0 && str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
 		i--;
 	return (i + 1);
 }
 
-int	ft_find_last(char **argv, int i)
+int ft_find_last_char(char *str, int i)
 {
-	while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\0'))
+	while (str[i] == '\0' || str[i] == ' ' || str[i] == '\t')
 		i--;
 	return (i);
 }
 
-int	ft_find_null(char **argv, int i)
+int ft_find_str_end(char *str, int i)
 {
-	while (argv[1][i])
+	while (str[i])
 		i++;
 	return (i);
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	i = 0;
-	int	j = 0;
+	int i = 0;
+	int j = 0;
 
 	if (argc == 2)
 	{
-		i = ft_find_null(argv, i);
-		i = ft_find_last(argv, i);
-		if (i == 0 || argv[1][i - 1] == ' ')
-			return (write (1, "\n", 1), 0);
-		j = ft_find_first(argv, i);
+		i = ft_find_str_end(argv[1], i);
+		i = ft_find_last_char(argv[1], i);
+		j = ft_find_first_char(argv[1], i);
+
 		while (j <= i)
 		{
 			write (1, &argv[1][j], 1);
 			j++;
 		}
 	}
-	return (write(1, "\n", 1), 0);
+	return (write (1, "\n", 1), 0);
 }
